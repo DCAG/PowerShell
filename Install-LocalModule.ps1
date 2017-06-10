60a218953fdf47ccb61747127bd059e7c7ab024a
Amir Granot <digiclick@hotmail.com>
2017-06-10 12:51:35 +0300

    remove forced verbose

--- Install-LocalModule.ps1
+++ Install-LocalModule.ps1
@@ -7,8 +7,9 @@ param(
 
 $moduleFolder   = Resolve-Path $Path
 $moduleName     = Split-Path $moduleFolder -Leaf
-$moduleManifest = Import-PowerShellDataFile $moduleFolder\$moduleName.psd1 -Verbose # Import-PowerShellDataFile (PowerShell version 5)
+$moduleManifest = Import-PowerShellDataFile $moduleFolder\$moduleName.psd1 # Import-PowerShellDataFile (PowerShell version 5)
+
 Write-Verbose "Module `"$moduleName`" version is $($moduleManifest.ModuleVersion)"
 
 mkdir "$env:ProgramFiles\WindowsPowerShell\Modules\$moduleName\$($moduleManifest.ModuleVersion)" -Force
-Copy-Item $moduleFolder\* -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\$moduleName\$($moduleManifest.ModuleVersion)\" -Force -Verbose
+Copy-Item $moduleFolder\* -Destination "$env:ProgramFiles\WindowsPowerShell\Modules\$moduleName\$($moduleManifest.ModuleVersion)\" -Force
