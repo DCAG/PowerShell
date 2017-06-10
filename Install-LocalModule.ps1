67ecb3f3682e21f003436b552c8db01605555e15
Amir Granot <digiclick@hotmail.com>
2017-06-10 12:48:54 +0300

    replaced get-item with resolve-path

--- Install-LocalModule.ps1
+++ Install-LocalModule.ps1
@@ -5,9 +5,9 @@ param(
     [ValidateScript({(Test-Path $_ -PathType Container) -and (Get-ChildItem $_\*.psd1)})]
     [string]$Path)
 
-$moduleFolder   = (Get-Item -Path $Path).FullName
-$moduleName     = Split-Path $moduleFolder.FullName -Leaf
-$moduleManifest = Import-PowerShellDataFile $moduleFolder\$moduleName.psd1 -Verbose
+$moduleFolder   = Resolve-Path $Path
+$moduleName     = Split-Path $moduleFolder -Leaf
+$moduleManifest = Import-PowerShellDataFile $moduleFolder\$moduleName.psd1 -Verbose # Import-PowerShellDataFile (PowerShell version 5)
 Write-Verbose "Module `"$moduleName`" version is $($moduleManifest.ModuleVersion)"
 
 mkdir "$env:ProgramFiles\WindowsPowerShell\Modules\$moduleName\$($moduleManifest.ModuleVersion)" -Force
